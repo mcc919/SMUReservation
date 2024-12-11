e2d9d577a1db93129ebbd25baea46c514c77ff4d
Ryan Han <mcc919@naver.com>
2024-12-12 08:38:32 +0900

    app.js etc

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { TextInput, StyleSheet, View, Text, TouchableOpacity, Button } from "react-native";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { theme } from "./color.js"
+import { parse } from 'htmlparser2';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [studentId, setStudentId] = useState("");
+    const [studentPw, setStudentPw] = useState("");
+
+    const onChangeStudentId = (data) => { setStudentId(data); console.log(studentId)}
+    const onChangeStudentPw = (data) => { setStudentPw(data); console.log(studentPw)}
+
+    const login = () => {
+
+
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.loginBox}>
+                <FontAwesome5 name="user-alt" size={24} color="black" style={styles.loginIcon} />
+                <TextInput onChangeText={onChangeStudentId}>tete</TextInput>
+            </View>
+            <View style={styles.loginBox}>
+                <FontAwesome5 name="lock" size={24} color="black" style={styles.loginIcon} />
+                <TextInput onChangeText={onChangeStudentPw}>tete</TextInput>
+            </View>
+            <Button title="로그인" color="#841584" onPress={login}></Button>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loginBox: {
+        flexDirection: "row",
+        margin: 10,
+    },
+    loginIcon: {
+        marginHorizontal: 10,
+    }
 });
